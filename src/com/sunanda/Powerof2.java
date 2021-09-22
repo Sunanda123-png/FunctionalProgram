6dd9b2846b9eb870f33c2dca19e4ea73a8218e49
Sunanda123-png <sunanda02@gmail.com>
2021-09-23 01:58:06 +0530

    Checking the power of 2

---
+++ src/com/sunanda/Powerof2.java
@@ -0,0 +1,15 @@
+package com.sunanda;
+
+public class Powerof2 {
+    public static void main(String[] args)
+    {
+        int n=10;
+        int powerof2=1;
+        for(int i=1;i<=n;i++)
+        {
+            powerof2=2*powerof2;
+            System.out.println("Result:- "+powerof2 );
+        }
+        System.out.println("Result:- "+powerof2 );
+    }
+}
