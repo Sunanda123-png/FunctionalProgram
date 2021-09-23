50ca73132edd61d29954619bb96b439ce5d102e1
Sunanda123-png <sunanda02@gmail.com>
2021-09-24 00:25:55 +0530

    Two number are swapping

---
+++ src/com/sunanda/SwapTwonumber.java
@@ -0,0 +1,19 @@
+package com.sunanda;
+
+public class SwapTwonumber {
+    public static void main(String[] args)
+    {
+        int a=7;
+        int b=47;
+        int temp;
+        System.out.println("Before swapping:");
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After swapping:");
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+    }
+}
