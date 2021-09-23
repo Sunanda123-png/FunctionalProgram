40da653e8c4193b1cd7d73da92de4c7ce0266668
Sunanda123-png <sunanda02@gmail.com>
2021-09-24 00:12:31 +0530

    Adding remainder problem

---
+++ src/com/sunanda/QuitionRemainder.java
@@ -0,0 +1,13 @@
+package com.sunanda;
+
+public class QuitionRemainder {
+    public static void main(String[] args)
+    {
+        int dividend=29;
+        int divisor=3;
+        int quotient=(dividend/divisor);
+        int remainder=(dividend%divisor);
+        System.out.println("Quotient is:"+quotient);
+        System.out.println("Remainder is:"+remainder);
+    }
+}
